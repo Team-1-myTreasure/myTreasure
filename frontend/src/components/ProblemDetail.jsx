955adbc7751a51a84f9d8233150a48600d8f2149
HirokiObata <hiroki_obata@mail.toyota.co.jp>
2024-05-30 18:51:57 +0900

    add: MAP機能実装
    Co-authored-by: tmc-kitagawa <tmc-kitagawa@users.noreply.github.com>

--- frontend/src/components/ProblemDetail.jsx
+++ frontend/src/components/ProblemDetail.jsx
@@ -1,14 +1,28 @@
 import { useLocation } from "react-router-dom";
 import { useState } from "react";
+import { Map } from "./Map";
+import { createContext } from "react";
+
+export const locationContext = createContext();
 
 export const ProblemDetail = () => {
   const location = useLocation();
   const productId = location.state.productId;
   const [problems, setProblems] = useState([]);
+  const [latlng, setLatlng] = useState([]);
 
   return (
     <>
       <div>{problems.length + 1}</div>
+      <div>
+        <p>目的地を選択</p>
+        <div>
+          <locationContext.Provider value={[latlng, setLatlng]}>
+            <Map />
+          </locationContext.Provider>
+          {console.log({ latlng })}
+        </div>
+      </div>
       <form
         onSubmit={(e) => {
           e.preventDefault();
@@ -20,8 +34,8 @@ export const ProblemDetail = () => {
             ...problems,
             {
               distination_name: "ラグーナ蒲郡",
-              distination_latitude: 34.808623368597864,
-              distination_longtitude: 137.27308416141435,
+              distination_latitude: latlng[0],
+              distination_longtitude: latlng[1],
               question: question,
               correct_answer: correct,
               incorrect_answer: incorrect,
@@ -30,16 +44,8 @@ export const ProblemDetail = () => {
               product_id: productId,
             },
           ]);
-          // setProblems((prev) => {
-          //   console.log([...prev, { correct: correct }]);
-          //   return [];
-          // });
         }}
       >
-        <div>
-          <p>目的地を選択</p>
-          <div>map</div>
-        </div>
         <div>
           <p>この目的地のヒント</p>
           <input name="hint" type="text" placeholder="目的地のヒントを入力" />
