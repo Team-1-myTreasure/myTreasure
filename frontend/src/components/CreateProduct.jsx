18535eebe575440cc47112e84c1f2ea9c011b6c3
HirokiObata <hiroki_obata@mail.toyota.co.jp>
2024-05-30 14:26:15 +0900

    fix:createproductからproblemへ遷移時にstateでIDを持てるようになった
    Co-authored-by: tmc-kitagawa <tmc-kitagawa@users.noreply.github.com>

--- frontend/src/components/CreateProduct.jsx
+++ frontend/src/components/CreateProduct.jsx
@@ -1,9 +1,16 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export const CreateProduct = () => {
   const [title, setTitle] = useState("");
   const [productId, setProductId] = useState();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (productId !== undefined) {
+      navigate("/host/problemdetail", { state: { productId: productId } });
+    }
+  }, [productId]);
 
   return (
     <>
@@ -24,10 +31,15 @@ export const CreateProduct = () => {
             },
             body: JSON.stringify({ host_id: 1, product_name: title }),
           });
-          setProductId(res);
+          const targetId = await res.json();
+          setProductId(targetId[0].product_id);
+          console.log("targetId: ", targetId[0].product_id);
         }}
       >
-        <Link to="/host/problemdetail" state={{ productId: productId }}></Link>
+        {/* <Link to="/host/problemdetail" state={{ productId: productId }}>
+          ゲームを作る
+        </Link> */}
+        ゲームを作る
       </button>
       <button>
         <Link to="/host/allproducts">キャンセル</Link>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     "/host": "http://localhost:8080",
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/product": "http://localhost:8080",
+      "/problem": "http://localhost:8080",
+    },
+  },
 });
