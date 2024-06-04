e6c6e32d1ee130b078296fbc3dffb16770d0d399
hiroki-kon <hiroki-kon@users.noreply.github.com>
2024-06-04 13:29:39 +0900

    クイズ実装
    Co-authored-by: Ichihara-Soma <Ichihara-Soma@users.noreply.github.com>

--- frontend/src/pages/GestQuestion.jsx
+++ frontend/src/pages/GestQuestion.jsx
@@ -1,5 +1,52 @@
+import { useState } from "react";
+import { Card, Text, Button } from "@mantine/core";
+import { useParams } from "react-router-dom";
+import useSWR from "swr";
+import axios from "axios";
+
+const fetcher = (url) => axios.get(url).then((res) => res.data);
+
+const QuestionResult = ({ correct }) => {
+  return (
+    <>{correct === true ? <Text>正解です</Text> : <Text>不正解です</Text>}</>
+  );
+};
+
 export const GestQuestion = () => {
-    return (
-        <h1>問題ページです。</h1>
-    )
-}
+  const [correct, setCorrect] = useState(undefined);
+  const { productId } = useParams();
+  const { data, isLoading } = useSWR(`/api/products/${productId}`, fetcher);
+
+  return (
+    <Card shadow="sm" padding="lg" radius="md" withBorder>
+      <Text>問題</Text>
+      {correct !== undefined ? <QuestionResult correct={correct} /> : null}
+      {isLoading === false && correct == undefined && (
+        <>
+          <Text size="xl">{data.question}</Text>
+          {Math.floor(Math.random() * 101) % 2 === 0 ? (
+            <>
+              {" "}
+              <Button variant="filled" onClick={() => setCorrect(true)}>
+                {data.correctAnswer}
+              </Button>
+              <Button variant="light" onClick={() => setCorrect(false)}>
+                {data.incorrectAnswer}
+              </Button>
+            </>
+          ) : (
+            <>
+              {" "}
+              <Button variant="filled" onClick={() => setCorrect(false)}>
+                {data.incorrectAnswer}
+              </Button>
+              <Button variant="light" onClick={() => setCorrect(true)}>
+                {data.correctAnswer}
+              </Button>
+            </>
+          )}
+        </>
+      )}
+    </Card>
+  );
+};
