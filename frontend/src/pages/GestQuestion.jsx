ffa941a64e8a4dfb710e2813a5d21a9273377203
hiroki-kon <hiroki-kon@users.noreply.github.com>
2024-06-05 11:44:05 +0900

    フォーマット実施

--- frontend/src/pages/GestQuestion.jsx
+++ frontend/src/pages/GestQuestion.jsx
@@ -37,51 +37,51 @@ export const GestQuestion = () => {
       ) : null}
 
       <Card shadow="sm" padding="lg" radius="md" withBorder>
-      <Text>問題</Text>
-      {correct !== undefined ? <QuestionResult correct={correct} /> : null}
-      {isLoading === false && correct == undefined && (
-        <>
-          <Text size="xl">{data.question}</Text>
-          {Math.floor(Math.random() * 101) % 2 === 0 ? (
-            <>
-              {" "}
-              <Button
-                variant="filled"
-                color="#00492B"
-                onClick={() => setCorrect(true)}
-              >
-                {data.correctAnswer}
-              </Button>
-              <Button
-                variant="light"
-                color="#00492B"
-                onClick={() => setCorrect(false)}
-              >
-                {data.incorrectAnswer}
-              </Button>
-            </>
-          ) : (
-            <>
-              {" "}
-              <Button
-                variant="filled"
-                color="#00492B"
-                onClick={() => setCorrect(false)}
-              >
-                {data.incorrectAnswer}
-              </Button>
-              <Button
-                variant="light"
-                color="#00492B"
-                onClick={() => setCorrect(true)}
-              >
-                {data.correctAnswer}
-              </Button>
-            </>
-          )}
-        </>
-      )}
-    </Card>
+        <Text>問題</Text>
+        {correct !== undefined ? <QuestionResult correct={correct} /> : null}
+        {isLoading === false && correct == undefined && (
+          <>
+            <Text size="xl">{data.question}</Text>
+            {Math.floor(Math.random() * 101) % 2 === 0 ? (
+              <>
+                {" "}
+                <Button
+                  variant="filled"
+                  color="#00492B"
+                  onClick={() => setCorrect(true)}
+                >
+                  {data.correctAnswer}
+                </Button>
+                <Button
+                  variant="light"
+                  color="#00492B"
+                  onClick={() => setCorrect(false)}
+                >
+                  {data.incorrectAnswer}
+                </Button>
+              </>
+            ) : (
+              <>
+                {" "}
+                <Button
+                  variant="filled"
+                  color="#00492B"
+                  onClick={() => setCorrect(false)}
+                >
+                  {data.incorrectAnswer}
+                </Button>
+                <Button
+                  variant="light"
+                  color="#00492B"
+                  onClick={() => setCorrect(true)}
+                >
+                  {data.correctAnswer}
+                </Button>
+              </>
+            )}
+          </>
+        )}
+      </Card>
     </>
   );
 };
